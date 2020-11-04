b1176b009977ff06e9b5b3309c0ba77372362f19
prof.choi <prof.choi@sch.ac.kr>
2020-11-04 15:55:29 +0900

    implement multiply and divide

--- git_practice/list_ops.py
+++ git_practice/list_ops.py
@@ -11,3 +11,15 @@ def subtract(foo, bar):
         out.append(f - b)
     return out
 
+def multiply(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f * b)
+    return out
+
+def divide(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f / b)
+    return out
+
